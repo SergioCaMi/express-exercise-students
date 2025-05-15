f619023fac6ec4f785b33147e9c8ceecc296c5c8
SergioCaMi <sergiocami84@gmail.com>
2025-05-15 20:50:14 +0200

    done

--- server4.js
+++ server4.js
@@ -8,6 +8,11 @@ app.get("/check/:check", (req, res) => {
     res.send(`La palabra ${req.params.check} ${palindrome(req.params.check) ? "SI" : "NO"} es un palíndromo`);
 });
 
+app.use((req, res)=>{
+    res.status(404).send(`404. Resource not found`);
+});
+
+
 app.listen(3000, () => {
     console.log(`Escuchando peticiones en http://localhost:3000`);
 });
