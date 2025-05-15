ef333b01a3911a10d0feebc9c6efa9c5e1076a54
SergioCaMi <sergiocami84@gmail.com>
2025-05-15 20:48:20 +0200

    it 4 done

---
+++ server4.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const { palindrome } = require('./utils/palindrome/index.js');
+const app = express();
+
+app.get("/check/:check", (req, res) => {
+    console.log("Petición recibida");
+    res.send(`La palabra ${req.params.check} ${palindrome(req.params.check) ? "SI" : "NO"} es un palíndromo`);
+});
+
+app.listen(3000, () => {
+    console.log(`Escuchando peticiones en http://localhost:3000`);
+});
