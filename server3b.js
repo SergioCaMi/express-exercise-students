b7e385a58990838883d0b780cda24d71ce22014b
SergioCaMi <sergiocami84@gmail.com>
2025-05-15 19:42:13 +0200

    it 3b done

--- server3b.js
+++ server3b.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
     res.sendFile(indexRoute);
 });
 
+app.get('/patient/:idPatient', (req, res)=>{
+const file = path.join(__dirname, "server3b-files", `${req.params.idPatient}.pdf`);
+  console.log("Petición recibida");
+  res.sendFile(file);
+
+});
+
+
 app.listen(3001, ()=>{
     console.log(`Escuchando peticiones en http://localhost:3001`);
 });
